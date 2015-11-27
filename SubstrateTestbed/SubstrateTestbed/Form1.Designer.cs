bc0450b6f51b6e9453a90199afa440cfc7a1ae4b
omglolbah <github@omglolbah.net>
2015-11-27 07:44:25 +0100

    Initial commit

---
+++ SubstrateTestbed/SubstrateTestbed/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace SubstrateTestbed
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbChunks = new System.Windows.Forms.ProgressBar();
+            this.txtDebug = new System.Windows.Forms.TextBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // pbChunks
+            // 
+            this.pbChunks.Location = new System.Drawing.Point(93, 12);
+            this.pbChunks.Name = "pbChunks";
+            this.pbChunks.Size = new System.Drawing.Size(842, 23);
+            this.pbChunks.TabIndex = 0;
+            // 
+            // txtDebug
+            // 
+            this.txtDebug.Location = new System.Drawing.Point(12, 41);
+            this.txtDebug.Multiline = true;
+            this.txtDebug.Name = "txtDebug";
+            this.txtDebug.Size = new System.Drawing.Size(923, 347);
+            this.txtDebug.TabIndex = 1;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(12, 12);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerate.TabIndex = 2;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(947, 400);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.txtDebug);
+            this.Controls.Add(this.pbChunks);
+            this.Name = "Form1";
+            this.Text = "Hurr Durr generator thingymajigger";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar pbChunks;
+        private System.Windows.Forms.TextBox txtDebug;
+        private System.Windows.Forms.Button btnGenerate;
+    }
+}
+
---
+++ SubstrateTestbed/SubstrateTestbed/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+using Substrate;
+using Substrate.Core;
+
+namespace SubstrateTestbed
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+     
+        }
+        string worldPath = @"B:\Games\FeedTheBeast\FTBInfinity\minecraft\saves\New World";
+        void GenerateList()
+        {
+            StreamWriter wr = new StreamWriter(Path.Combine(Path.GetDirectoryName(Application.ExecutablePath), "OreList.txt"), false);
+
+            int totalcount = 0;
+            int ore = 2646;
+            int oreMeta = 6;
+
+            // Open our world
+
+            AnvilWorld world = AnvilWorld.Open(worldPath);
+
+            // The chunk manager is more efficient than the block manager for
+            // this purpose, since we'll inspect every block
+            RegionChunkManager rcm = world.GetChunkManager();
+
+            pbChunks.Maximum = rcm.Count();
+            pbChunks.Minimum = 0;
+            pbChunks.Value = 0;
+
+            foreach (ChunkRef chunk in rcm)
+            {
+                pbChunks.Value++;
+                if (chunk.X < 0 || chunk.Z < 0 || chunk.Z > 134 || chunk.X > 111)
+                {
+                    continue;
+                }
+
+                // You could hardcode your dimensions, but maybe some day they
+                // won't always be 16.  Also the CLR is a bit stupid and has
+                // trouble optimizing repeated calls to Chunk.Blocks.xx, so we
+                // cache them in locals
+                int xdim = chunk.Blocks.XDim;
+                int ydim = chunk.Blocks.YDim;
+                int zdim = chunk.Blocks.ZDim;
+
+
+                AnvilBiomeCollection chunkBiome = chunk.Biomes;
+
+                // x, z, y is the most efficient order to scan blocks (not that
+                // you should care about internal detail)
+                for (int x = 0; x < xdim; x++)
+                {
+                    for (int z = 0; z < zdim; z++)
+                    {
+                        int biome = chunkBiome.GetBiome(x, z);
+
+                        for (int y = 0; y < ydim; y++)
+                        {
+                            int cid = chunk.Blocks.GetID(x, y, z);
+                            if (y > 3)
+                            {
+                                if (cid == ore)
+                                {
+                                    int cdata = chunk.Blocks.GetData(x, y, z);
+                                    if (cdata == oreMeta)
+                                    {
+                                        wr.WriteLine("E;{0};{1};{2};{3}", chunk.X * xdim + x, chunk.Z * zdim + z, y,biome);
+                                        totalcount++;
+                                    }
+                                }
+                            }
+                            //Break when hitting daylight.
+                            int light = chunk.Blocks.GetSkyLight(x, y, z);
+                            if (light == 12)
+                            {
+                                break;
+                            }
+
+                        }
+                    }
+                }
+                if (pbChunks.Value % 32 == 0)
+                {
+                    txtDebug.Text = string.Format("Processed Chunk {0},{1}", chunk.X, chunk.Z) + Environment.NewLine;
+                    Application.DoEvents();
+                }
+            }
+            
+            txtDebug.Text += string.Format("Found ore count:{0}", totalcount) + Environment.NewLine;
+            wr.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GenerateList();
+        }
+
+    }
+}
